9bed09efe0f60eb2bf884a98a6bed0f66c152a73
Andreea <andreea.rosca204@gmail.com>
2021-03-26 22:02:48 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Typography } from '@material-ui/core';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-firsth">
+      <Typography variant='h2'>Andreea-Maria Roșca</Typography>
+      </div>
+      <Typography variant='h3'>Student</Typography>
+    </div>
+  );
+}
+
+export default App;
