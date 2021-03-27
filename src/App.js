0fc774fdb78f25a98a6749321c9c8c95c1d070bb
Andreea <andreea.rosca204@gmail.com>
2021-03-27 13:40:55 +0200

    practice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import { Typography } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
+import Box from './components/Box';
 
+const App = () => {
 
-function App() {
+  const color = "primary";
+
+        {/* 
+          const propsForButton = { 
+              color: color,
+              variant: 'contained',
+              disableRipple: true,
+              size: 'large'
+            }
+        */}
   return (
     <div className="App">
       <div className="App-firsth">
-      <Typography variant='h2'>Andreea-Maria Roșca</Typography>
+        <Typography variant='h2'>Andreea-Maria Rosca</Typography>
       </div>
       <Typography variant='h3'>Student</Typography>
+      <Button variant="contained" color={color} disableRipple ={true} size="large">
+        Please click me
+      </Button>
+      <Box name ="Andreea">
+        <h1> Click me again </h1>
+      </Box>
+
+    
     </div>
   );
 }
---
+++ src/components/Box.js
@@ -0,0 +1,68 @@
+import { Button, Input } from '@material-ui/core';
+import React from 'react';
+import './Box.css'
+
+// const Box = (props) => {
+//     return(
+//         <div>Salut {props.name} </div>
+//     )
+// }
+
+class Box extends React.Component {
+    render() {
+        console.log(this.props);
+        console.log(this.props.children);
+
+        const props = this.props; // { name: 'Andreea', children: 'Si mai smechera' }
+
+        return(
+            <div className="mylittlebox"> 
+                {/* <div> Salut {props.name} </div> 
+                <div>
+                    {props.children}
+                </div>
+                <Form /> */}
+                <div className="flex-container">
+                    <div className="flex-child">1</div>
+                    <div className="flex-child">2</div>
+                    <div className="big-flex-child">3</div>
+                </div>
+            </div>
+        )   
+    }
+}
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            disableTheButton: true
+        }
+    }
+
+    onChange = (event) => {
+        const value = event.target.value;
+        console.log(value);
+
+        if (value.length == 10) { // disableTheButton: false
+            // this.state.disableTheButton = false;
+            this.setState({ disableTheButton: false });
+        }
+        else{
+            this.setState({ disableTheButton: true });
+
+        }
+
+    }
+
+    render() {
+        console.log('render');
+
+        return (<div> 
+            <Input onChange={this.onChange} />
+            <Button disabled={this.state.disableTheButton}> Submit </Button>
+        </div>);
+    }
+}
+
+export default Box;
